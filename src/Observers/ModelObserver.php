fe524bbcbd8eaaa2310690b78edaf77b2189b4f4
Cy <cyrilschool12@gmail.com>
2024-05-22 11:48:24 +0800

    Support Multilevel Models

--- src/Observers/ModelObserver.php
+++ src/Observers/ModelObserver.php
@@ -5,14 +5,16 @@ namespace Marjose123\FilamentWebhookServer\Observers;
 use Illuminate\Database\Eloquent\Model;
 use Marjose123\FilamentWebhookServer\HookJobProcess;
 use Marjose123\FilamentWebhookServer\Models\FilamentWebhookServer;
+use ReflectionClass;
 use Spatie\ModelInfo\ModelInfo;
 
 class ModelObserver
 {
     public function created(Model $model)
     {
-        $modelInfo = ModelInfo::forModel($model::class);
-        $module = ucfirst(str_replace("App\Models\\", '', $modelInfo->class));
+//        $modelInfo = ModelInfo::forModel($model::class);
+//        $module = ucfirst(str_replace("App\Models\\", '', $modelInfo->class));
+        $module = ucfirst((new ReflectionClass($model))->getShortName());
         /*
          * Search on the DB that want to receive webhook from this model
          */
