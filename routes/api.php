a706451f1b7be6fcb229afe4f7aeb06e0ce98370
Cy <cyrilschool12@gmail.com>
2024-05-22 13:05:50 +0800

    Chore (remove unnecessary files/codes)

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Marjose123\FilamentWebhookServer\Http\Controllers\WebhookController;
-
-Route::prefix('/webhook-server/api')->group(function () {
-    Route::post('/', [WebhookController::class, 'create']);
-});
