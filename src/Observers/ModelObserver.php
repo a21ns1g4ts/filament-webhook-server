36085a668d276426da6a60dc8a44d7a1f56e7c3d
Poseidon281 <62171904+Poseidon281@users.noreply.github.com>
2024-07-22 12:51:28 +0200

    Revert "Optimizing observer"
    This reverts commit a280c83edfdeb6021674e1b9c1bc0b8eb19a3e3e.

--- src/Observers/ModelObserver.php
+++ src/Observers/ModelObserver.php
@@ -3,7 +3,6 @@
 namespace Marjose123\FilamentWebhookServer\Observers;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Cache;
 use Marjose123\FilamentWebhookServer\HookJobProcess;
 use Marjose123\FilamentWebhookServer\Models\FilamentWebhookServer;
 use ReflectionClass as RC;
@@ -11,54 +10,72 @@ use Spatie\ModelInfo\ModelInfo;
 
 class ModelObserver
 {
-    protected function getModuleName($model)
-    {
-        $modelInfo = ModelInfo::forModel($model::class);
-        return ucfirst(str_replace("App\\Models\\", '', $modelInfo->class));
-    }
-
-    protected function getWebhooks($eventType, $module)
-    {
-        return Cache::remember("webhooks_{$eventType}_{$module}", 60, function () use ($eventType, $module) {
-            return FilamentWebhookServer::query()
-                ->whereJsonContains('events', [$eventType])
-                ->where('model', $module)
-                ->get();
-        });
-    }
-
     public function created(Model $model)
     {
-        $module = $this->getModuleName($model);
-        $search = $this->getWebhooks('created', $module);
+        $module = ucfirst((new RC($model))->getShortName());
+        /*
+         * Search on the DB that want to receive webhook from this model
+         */
+        $search = FilamentWebhookServer::query()->whereJsonContains('events', ['created'])->where('model', '=', $module)->get();
+        /*
+         * Send to Job Process
+         */
         (new HookJobProcess($search, $model, 'created', $module))->send();
     }
 
     public function updated(Model $model)
     {
-        $module = $this->getModuleName($model);
-        $search = $this->getWebhooks('updated', $module);
+        $modelInfo = ModelInfo::forModel($model::class);
+        $module = ucfirst(str_replace("App\Models\\", '', $modelInfo->class));
+        /*
+         * Search on the DB that want to receive webhook from this model
+         */
+        $search = FilamentWebhookServer::query()->whereJsonContains('events', ['updated'])->where('model', '=', $module)->get();
+        /*
+         * Send to Job Process
+         */
         (new HookJobProcess($search, $model, 'updated', $module))->send();
     }
 
     public function deleted(Model $model)
     {
-        $module = $this->getModuleName($model);
-        $search = $this->getWebhooks('deleted', $module);
+        $modelInfo = ModelInfo::forModel($model::class);
+        $module = ucfirst(str_replace("App\Models\\", '', $modelInfo->class));
+        /*
+         * Search on the DB that want to receive webhook from this model
+         */
+        $search = FilamentWebhookServer::query()->whereJsonContains('events', ['deleted'])->where('model', '=', $module)->get();
+        /*
+         * Send to Job Process
+         */
         (new HookJobProcess($search, $model, 'deleted', $module))->send();
     }
 
     public function restored(Model $model)
     {
-        $module = $this->getModuleName($model);
-        $search = $this->getWebhooks('restored', $module);
+        $modelInfo = ModelInfo::forModel($model::class);
+        $module = ucfirst(str_replace("App\Models\\", '', $modelInfo->class));
+        /*
+         * Search on the DB that want to receive webhook from this model
+         */
+        $search = FilamentWebhookServer::query()->whereJsonContains('events', ['restored'])->where('model', '=', $module)->get();
+        /*
+         * Send to Job Process
+         */
         (new HookJobProcess($search, $model, 'restored', $module))->send();
     }
 
     public function forceDeleted(Model $model)
     {
-        $module = $this->getModuleName($model);
-        $search = $this->getWebhooks('forceDeleted', $module);
+        $modelInfo = ModelInfo::forModel($model::class);
+        $module = ucfirst(str_replace("App\Models\\", '', $modelInfo->class));
+        /*
+         * Search on the DB that want to receive webhook from this model
+         */
+        $search = FilamentWebhookServer::query()->whereJsonContains('events', ['forceDeleted'])->where('model', '=', $module)->get();
+        /*
+         * Send to Job Process
+         */
         (new HookJobProcess($search, $model, 'forceDeleted', $module))->send();
     }
 }
